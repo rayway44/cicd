f916d8b66b34828eafcdf9847a9815c1dfc3688d
Ray Letoa <rayletoa@RAYway-2.hub>
2022-03-02 13:36:51 +1300

    have put stuart back

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        we have changed this file
+        <h2>Getting started with React testing library learn react stuart no deal</h2>
       </header>
     </div>
   );
 }
-
 export default App;
