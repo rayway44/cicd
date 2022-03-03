aaa7bd4bf0c8a11771653243090cd98486a98207
Ray Letoa <rayletoa@RAYway-2.hub>
2022-03-03 20:50:52 +1300

    added a header component

--- app/src/App.js
+++ app/src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
 import "./App.css";
+import Header from "./Components/Header"
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        
         <h2>Getting started with React testing library learn react stuart no deal<br/> I have added #RAYway</h2>
+        <Header />
       </header>
     </div>
   );
