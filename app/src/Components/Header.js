311070d783a27d4758722d448ba539bd5c2d14aa
Ray Letoa <rayletoa@RAYway-2.hub>
2022-03-03 23:07:34 +1300

    we have added more out unit test text stripper

--- app/src/Components/Header.js
+++ app/src/Components/Header.js
@@ -5,14 +5,44 @@ export default function Header() {
 
 console.log('we are hitting the header')
 
+function watsonTester(){
+
+  // put value of from textbox in variable question
+  let question = document.getElementById('textBox').value
+
+  // take question and replace an of special characters
+  let trimmer = question.replace(/[$/!/@/#/$/%/^/&/*/+/{/}/[/:/;/?//>/</]/g, "")
+
+  // the question and trim any white space
+  let finalQuestion = trimmer.trim();
+
+  // check if final question is trimmed correctly 
+  console.log(finalQuestion);
+
+  // get box and clear it out
+  let demoBox = document.getElementById('demoBox')
+  // demoBox.innerHTML = ""
+
+  demoBox.innerHTML = `<p>Searching. . . . ${finalQuestion} </p>`
+
+}
+
+function clicker(){
+  console.log('youve hit me now what')
+}
+
   return (
     <div>
       
-      <div id='header-box'>
-      This is app Header homie
+      <img src="./Images/turners-logo.png" />
+      <h1>FAQ Search</h1>
+      <input type="text" id="textBox" placeholder="e.g. what fees are there ?" />
+
+      <button onClick={watsonTester}>Search</button>
+
+      <div id="demoBox" class="results">
       </div>
 
     </div>
-
   )
 }
