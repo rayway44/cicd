9b2daa6181d54e54b37fe8d37c6736df3f582267
Ray Letoa <rayletoa@RAYway-2.hub>
2022-03-03 22:12:53 +1300

    we got some styling on header now

--- app/src/Components/Header.js
+++ app/src/Components/Header.js
@@ -1,7 +1,18 @@
 import React from 'react'
+import './CSS/Header.css';
 
 export default function Header() {
+
+console.log('we are hitting the header')
+
   return (
-    <div>This is app Header homie</div>
+    <div>
+      
+      <div id='header-box'>
+      This is app Header homie
+      </div>
+
+    </div>
+
   )
 }
