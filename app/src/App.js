80ef5ea1af2df1e989da0fbf66a6a301ed6f8b0e
Ray Letoa <rayletoa@RAYway-2.hub>
2022-03-03 20:59:07 +1300

    pushing in new line

--- app/src/App.js
+++ app/src/App.js
@@ -9,6 +9,7 @@ function App() {
         
         <h2>Getting started with React testing library learn react stuart no deal<br/> I have added #RAYway</h2>
         <Header />
+        This is kicking 
       </header>
     </div>
   );
